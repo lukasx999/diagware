757b813f578868020a0da12cfc6fe1f97c581518
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2024-12-27 12:39:00 +0100

    .

--- legacy/ui/components/documents.rs
+++
@@ -1,8 +0,0 @@
-
-    pub fn ui_documents(&mut self, ui: &mut egui::Ui) {
-
-        ui.label("Documents");
-        // TODO: fetch and render list of documents from database
-        ui.button("Download");
-
-    }
